a9bc136509f2343a5b3cd0b8449302c226193b11
ferind07 <96059330+ferind07@users.noreply.github.com>
2023-11-09 09:50:14 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# coding test
+
+requirements
+
+-   php 8.1.11
+-   laravel 10
+
+run the project
+
+```bash
+php artisan serve
+```
---
+++ resources/views/view.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coding test</title>
+    <script src="/js/jquery-3.7.1.min.js"></script>
+    <script src="/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="/dist/css/bootstrap.min.css">
+    <script></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <h3 class="mb-0">Coding test</h3>
+        <hr />
+        <div class="row">
+            <div class="col-2">
+                <input type="number" name="angka" id="angka" class="form-control">
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
